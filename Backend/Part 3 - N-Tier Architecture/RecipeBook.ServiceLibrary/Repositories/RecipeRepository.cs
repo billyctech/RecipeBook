c1d3050508c0fba4cded3db3a9b3a34276983f63
Billy Chen <billyctech@gmail.com>
2020-05-24 18:43:46 -0400

    Added Part 3 - N-Tier Architecture

---
+++ Backend/Part 3 - N-Tier Architecture/RecipeBook.ServiceLibrary/Repositories/RecipeRepository.cs
@@ -0,0 +1,15 @@
+﻿using RecipeBook.ServiceLibrary.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecipeBook.ServiceLibrary.Repositories
+{
+	public class RecipeRepository
+	{
+		public void SaveRecipeToDatabase(RecipeEntity recipeEntity)
+		{
+			// Save to Database
+		}
+	}
+}
